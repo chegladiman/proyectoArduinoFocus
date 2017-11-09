e6f433c86aa9394199ac5fb867e58ad3b5650f1e
Facundo Dominguez <facundoariel.dominguez@gmail.com>
2017-11-08 22:09:23 -0300

    Add files via upload

---
+++ ProyectoFocus.ino
@@ -0,0 +1,133 @@
+#include<Servo.h>
+Servo myservo; // create servo object to control servo
+int Echo = A4;  
+int Trig = A5;
+int in1 = 6;
+int in2 = 7;
+int in3 = 8;
+int in4 = 9;
+int ENA = 5;
+int ENB = 11;
+int ABS = 170;
+int distancia;
+
+
+void setup(){
+
+  myservo.attach(3);// attach servo on pin 3 to servo object
+  Serial.begin(9600);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+
+  
+  _mForward();
+}
+
+
+
+  void _mForward()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+  digitalWrite(in1,HIGH);//digital output
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+ Serial.println("go forward!");
+}
+
+void _mBack()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+ Serial.println("go back!");
+}
+
+void _mleft()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW); 
+ Serial.println("go left!");
+}
+
+void _mright()
+{
+ analogWrite(ENA,ABS);
+ analogWrite(ENB,ABS);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+ Serial.println("go right!");
+} 
+void _mStop()
+{
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+  Serial.println("Stop!");
+}
+
+ /*Ultrasonic distance measurement Sub function*/
+int Distance_test()   
+{
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance/58;       
+  return (int)Fdistance;  
+}
+
+void loop(){
+  /*
+  _mForward();
+  delay(250);
+   _mStop();
+  _mright();
+  delay(250);
+  _mStop();
+  _mForward();
+  delay(250);
+  _mStop();
+  _mleft();
+  delay(500);
+  _mStop();
+  */
+
+
+
+
+  distancia=Distance_test();
+  if(distancia<30){
+
+     _mleft();
+     delay(500);
+     _mForward();
+     _mStop();
+  
+  
+  }
+}
+  
+
+  
+
+
+
+
